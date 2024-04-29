bdcb4c0ae73471553a11b34d68750f1986caa111
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-04-28 22:17:43 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+// const { User, Thought, Reaction } = require('./models');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Built in Express function that parses incoming requests to JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
